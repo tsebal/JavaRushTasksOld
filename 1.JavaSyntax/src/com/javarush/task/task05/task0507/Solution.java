a62864ac423842b1de1f32601a71e5bb142d652e
Tsebal <atsebal@gmail.com>
2019-02-01 16:53:16 +0300

    task 0507 solved

--- 1.JavaSyntax/src/com/javarush/task/task05/task0507/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0507/Solution.java
@@ -4,9 +4,23 @@ package com.javarush.task.task05.task0507;
 Среднее арифметическое
 */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        int sum = 0, i = 0;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            int number = Integer.parseInt(reader.readLine());
+            if (number == -1)
+                break;
+            sum += number;
+            i++;
+        }
+
+        System.out.println((float) sum/i);
     }
 }
 
