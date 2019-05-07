2046c55357fc7e11279e1f997265e86bbfe266c4
Alex Tse <atsebal@gmail.com>
2019-05-07 12:47:31 +0300

    Task solved

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1617/Solution.java
@@ -0,0 +1,38 @@
+package com.javarush.task.task16.task1617;
+
+/* 
+Отсчет на гонках
+*/
+
+public class Solution {
+    public static volatile int numSeconds = 3;
+
+    public static void main(String[] args) throws InterruptedException {
+        RacingClock clock = new RacingClock();
+        Thread.sleep(3500);
+        clock.interrupt();
+    }
+
+    public static class RacingClock extends Thread {
+        public static int numSeconds = 4;
+
+        public RacingClock() {
+            start();
+        }
+
+        public void run() {
+            try {
+                while (numSeconds >= 1) {
+                    System.out.print(numSeconds + " ");
+                    numSeconds--;
+                    Thread.sleep(1000);
+                }
+            }
+            catch (InterruptedException e) {
+                System.out.println("Прервано!");
+                return;
+            }
+            System.out.print("Марш!");
+        }
+    }
+}
